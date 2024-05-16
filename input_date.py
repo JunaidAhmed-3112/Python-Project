abdb34d29701371e299c51132f9196e70271db92
Junaid Ahmed <mohammedjunaidahmed3112@gmail.com>
2024-05-16 16:35:59 +0530

    leet code files are in functions.py

---
+++ input_date.py
@@ -0,0 +1,4 @@
+name=input("Enter your Name:")
+print("welcome",name)
+Age=int(input("Enter your Age:"))
+print("your age is",Age)
---
+++ swapping_without_3rd_element.py
@@ -0,0 +1,8 @@
+a=10  #masala in a
+b=20  #chicken in b
+a = a+b  #a=30
+b = a-b  #b=10
+a = a-b  #a=20
+print(a,b)
+
+
